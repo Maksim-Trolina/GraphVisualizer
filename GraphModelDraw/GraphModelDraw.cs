c2117b9c024f4ade6a078699ebfda2612fb2819e
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-09-11 00:47:28 +0300

    Create GraphModelDraw

---
+++ GraphModelDraw/GraphModelDraw.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+
+namespace GraphModelDraw
+{
+    public class GraphModelDraw
+    {
+        public readonly Brush BrushCircle;
+
+        public readonly Brush TextBrush;
+
+        public readonly float X;
+
+        public readonly float Y;
+
+        public readonly float Width;
+
+        public readonly float Height;
+
+        public readonly string Text;
+    }
+}
