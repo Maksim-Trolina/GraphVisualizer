2f5947154a6266456b4028a4894e4180bfc884e5
DONBASS <muhomossyas@gmail.com>
2020-10-01 21:23:22 +0300

    Tests for a CollisionVertex

---
+++ CollisionDrawTest/CollisionVertexTest.cs
@@ -0,0 +1,72 @@
+using NUnit.Framework;
+using CollisionDraw;
+using System;
+using System.Collections.Generic;
+using GraphModelDraw;
+
+namespace CollisionDrawTest
+{
+    public class CollisionVertexTest
+    {
+        private List<VertexDraw> vertexDraws;
+
+        private VertexDraw vertexDraw;
+
+        private CollisionVertex collisionVertex;
+
+        [SetUp]
+        public void Setup()
+        {
+            vertexDraws = new List<VertexDraw>();
+
+            collisionVertex = new CollisionVertex();
+
+            float x = 30.5f;
+            float y = 20.7f;
+
+            vertexDraw = new VertexDraw(BrushColor.Red, BrushColor.Green, x, y, (float)VertexParameters.Width, (float)VertexParameters.Height, "", vertexDraws.Count);
+        }
+
+        [Test]
+        public void IsDrawVertexTest_EmptyList_TrueExpected()
+        {
+            vertexDraws.Clear();
+
+            bool actual = collisionVertex.IsDrawVertex(vertexDraw, vertexDraws);
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsDrawVertexTest_NoCollision_TrueExpected()
+        {
+            float x = vertexDraw.X + 2 * (float)VertexParameters.Radius + 0.1f;
+            float y = vertexDraw.Y;          
+
+            VertexDraw vertexItemList = new VertexDraw(BrushColor.Red, BrushColor.Green, x, y, (float)VertexParameters.Width 
+               , (float)VertexParameters.Height, "", vertexDraws.Count);
+
+            vertexDraws.Add(vertexItemList);
+
+            bool actual = collisionVertex.IsDrawVertex(vertexDraw, vertexDraws);
+
+            Assert.IsTrue(actual);
+        }
+
+        [Test]
+        public void IsDrawVertexTest_IsCollision_FalseExpected()
+        {
+            float x = vertexDraw.X + 2 * (float)VertexParameters.Radius - 0.1f;
+            float y = vertexDraw.Y;
+
+            VertexDraw vertexItemList = new VertexDraw(BrushColor.Red, BrushColor.Green, x, y, (float)VertexParameters.Width
+               , (float)VertexParameters.Height, "", vertexDraws.Count);
+
+            vertexDraws.Add(vertexItemList);
+
+            bool actual = collisionVertex.IsDrawVertex(vertexDraw, vertexDraws);
+
+            Assert.IsFalse(actual);
+        }
+    }
+}
