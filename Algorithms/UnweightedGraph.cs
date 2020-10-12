96424e7e43053fbf49d0b27fbbbb509325085bff
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-10-13 01:36:27 +0300

    Add methods

--- Algorithms/UnweightedGraph.cs
+++ Algorithms/UnweightedGraph.cs
@@ -1,8 +1,21 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Algorithms
 {
     public class UnweightedGraph
     {
+        private List<List<int>> graph;
+
+        public UnweightedGraph(List<List<int>> graph)
+        {
+            this.graph = graph;
+        }
+
+        
+        private void DFS()
+        {
+
+        }
     }
 }
