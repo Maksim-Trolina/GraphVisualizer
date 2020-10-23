88102db0c9653d58376f35e5f177d68b6e8a544f
DONBASS <muhomossyas@gmail.com>
2020-10-24 01:51:31 +0300

    create adjacency list

---
+++ GraphRepresentation/AdjacencyList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using CraphModel;
+using GraphModelDraw;
+
+namespace GraphRepresentation
+{
+    public class AdjacencyList
+    {
+        Dictionary<int, List<Node>> adjacencyList;
+
+        public AdjacencyList(List<Vertex> verticles)
+        {
+            adjacencyList = new Dictionary<int, List<Node>>();
+
+            foreach (var vertex in verticles)
+            {
+                AddVertex(vertex);
+            }
+        }
+
+        public void AddVertex(Vertex vertex)
+        {
+            if (!adjacencyList.ContainsKey(vertex.Id))
+            {
+                adjacencyList.Add(vertex.Id, vertex.Nodes);
+            }
+
+        }
+
+        public void AddNode(Vertex vertexStart, Vertex vertexEnd, int weight)
+        {
+
+            Node node = new Node();
+
+            node.Connectable = vertexEnd.Id;
+
+            node.Weight = weight;
+
+            adjacencyList[vertexStart.Id].Add(node);
+        }
+    }
+}
