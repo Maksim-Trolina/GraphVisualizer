fccba12830a1ec6222937ccee62404de4fd58aa1
Yaropolk <maks.panasenko01@bk.ru>
2020-11-08 21:26:32 +0300

    Create CycleButton

---
+++ Forms/DrawForm/CycleButton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Forms.DrawForm
+{
+    public class CycleButton : ToolStripButton
+    {
+
+    }
+}
