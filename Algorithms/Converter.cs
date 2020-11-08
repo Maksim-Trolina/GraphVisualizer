e1365f10ac3fe13c131652ee938c1575446421d8
Yaropolk <maks.panasenko01@bk.ru>
2020-11-08 19:44:04 +0300

    Completed Converter

--- Algorithms/Converter.cs
+++ Algorithms/Converter.cs
@@ -9,7 +9,19 @@ namespace Algorithms
     {
         public List<List<int>> ConvertToSimpleGraph(AdjacencyList adjacencyList)
         {
+            List<List<int>> graph = new List<List<int>>();
 
+            for(int i = 0; i < adjacencyList.adjacencyList.Count; i++)
+            {
+                graph.Add(new List<int>());
+
+                for(int j = 0; j < adjacencyList.adjacencyList[i].Count; j++)
+                {
+                    graph[i].Add(adjacencyList.adjacencyList[i][j].Connectable);
+                }
+            }
+
+            return graph;
         }
     }
 }
