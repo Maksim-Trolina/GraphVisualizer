ec8453f1a17e2f181bff23d5f882e19772bdb195
Yaropolk <maks.panasenko01@bk.ru>
2020-11-08 22:31:35 +0300

    Create CycleButton

--- Forms/DrawForm/CycleButton.cs
+++ Forms/DrawForm/CycleButton.cs
@@ -7,6 +7,32 @@ namespace Forms.DrawForm
 {
     public class CycleButton : ToolStripButton
     {
+        private bool isShowCycle;
+        public CycleButton(int width,int height)
+        {
+            Size = new System.Drawing.Size(width, height);
 
+            Dock = DockStyle.Top;
+
+            Text = "Show Cycle";
+
+            Click += new EventHandler(ButtonClick);
+
+            isShowCycle = false;
+        }
+
+        public void ButtonClick(object sender,EventArgs e)
+        {
+            isShowCycle = !isShowCycle;
+
+            if (isShowCycle)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
     }
 }
