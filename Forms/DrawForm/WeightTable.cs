f093097ada03e4a6eeea1dc154de98cf7dd3216b
EugenKoulik <62559389+EugenKoulik@users.noreply.github.com>
2020-11-11 19:49:24 +0300

    Fixed scrolling huinya

--- Forms/DrawForm/WeightTable.cs
+++ Forms/DrawForm/WeightTable.cs
@@ -84,7 +84,8 @@ namespace Forms.DrawForm
                 for (int j = countCellsBefore; j < countCellsBefore + countOfNewVertexs; j++)
                 {
 
-                    matrix[i].Add(new InputCountBox(width, height, positionX + (width + stepX) * j, positionY + (height + stepY) * i));
+                    matrix[i].Add(new InputCountBox(width, height, positionX + (width + stepX) * i - weightTable.HorizontalScroll.Value
+                        , positionY + (height + stepY) * j - weightTable.VerticalScroll.Value));
 
                     if (i == j)
                     {
@@ -103,8 +104,9 @@ namespace Forms.DrawForm
 
                 for(int j = 0; j < countOfNewVertexs + countCellsBefore; j++)
                 {
-                 
-                    matrix[i].Add(new InputCountBox(width, height, positionX + (width + stepX) * j, positionY + (height + stepY) * i));
+
+                    matrix[i].Add(new InputCountBox(width, height, positionX + (width + stepX) * i - weightTable.HorizontalScroll.Value
+                         , positionY + (height + stepY) * j - weightTable.VerticalScroll.Value));
 
                     if (i == j)
                     {
