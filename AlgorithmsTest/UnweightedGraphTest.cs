4e81ad01b50306a145f6557ce3465535d0726256
Yaropolk <maks.panasenko01@bk.ru>
2020-11-26 00:27:54 +0300

    Completed write test

--- AlgorithmsTest/UnweightedGraphTest.cs
+++ AlgorithmsTest/UnweightedGraphTest.cs
@@ -100,5 +100,19 @@ namespace AlgorithmsTest
 
             Assert.IsTrue(isTrueCount && cycles[0].SequenceEqual(new List<int> { 0, 1, 2, 0 }) && cycles[1].SequenceEqual(new List<int> { 0, 4, 1, 3, 0 }));
         }
+
+        [Test]
+        public void GetCyclesTest_GraphWithoutCycles_ZeroCycle()
+        {
+            List<List<int>> graph = new List<List<int>> { new List<int> { 1 }, new List<int> { 2 }, new List<int>() };
+            UnweightedGraph unweightedGraph = new UnweightedGraph(graph);
+
+            List<List<int>> cycles = unweightedGraph.GetCycles();
+
+            bool isTrueCount = cycles.Count == 0;
+
+            Assert.IsTrue(isTrueCount);
+            
+        }
     }
 }
