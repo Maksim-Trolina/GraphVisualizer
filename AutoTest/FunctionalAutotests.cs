a78253bc7a294d113261f914cde4231eda048ea5
EugenKoulik <62559389+EugenKoulik@users.noreply.github.com>
2020-12-16 05:17:33 +0300

    Create and load graph tests

---
+++ AutoTest/FunctionalAutotests.cs
@@ -0,0 +1,92 @@
+using NUnit.Framework;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+using System;
+using System.IO;
+using OpenQA.Selenium.Appium;
+using System.Diagnostics;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium;
+
+
+namespace AutoTest
+{
+    public class Tests
+    {
+
+        protected const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723/";
+
+        private const string WpfAppId = @"D:\Eugene27\GraphVisualizer\Forms\bin\Debug\netcoreapp3.1\Forms.exe";
+
+        protected static RemoteWebDriver session;
+
+
+        [Test]
+        public void CreateNewGraph_Draw_Test()
+        {
+            AppiumOptions opt = new AppiumOptions();
+
+            opt.AddAdditionalCapability("app", WpfAppId);
+
+            session = new RemoteWebDriver(new Uri(WindowsApplicationDriverUrl), opt);
+          
+
+            session.FindElementByName("Create new graph").Click();
+
+            session.SwitchTo().Window(session.WindowHandles[0]);
+
+            session.FindElementByName("Create vertexes").Click();
+
+            session.SwitchTo().Window(session.WindowHandles[0]);
+
+            Actions builder = new Actions(session); 
+
+
+            builder.MoveToElement(session.FindElementByName("Weight Table"), 350 , 50 ).Click();
+
+            builder.MoveToElement(session.FindElementByName("Weight Table"), 350, 300).Click();
+
+            builder.MoveToElement(session.FindElementByName("Weight Table"), 550, 300).Click();
+
+
+            builder.MoveToElement(session.FindElementByName("Weight Table"), 350, 50).Click();
+
+            builder.MoveToElement(session.FindElementByName("Weight Table"), 350, 300).Click();
+
+            builder.MoveToElement(session.FindElementByName("Weight Table"), 350, 300).Click();
+
+            builder.MoveToElement(session.FindElementByName("Weight Table"), 550, 300).Click();
+
+            builder.MoveToElement(session.FindElementByName("Weight Table"), 550, 300).Click();
+
+            builder.MoveToElement(session.FindElementByName("Weight Table"), 350, 50).Click().Perform();
+
+            session.Quit();
+        }
+
+        [Test]
+
+        public void GraphLoading_Test()
+        {
+
+            AppiumOptions opt = new AppiumOptions();
+
+            opt.AddAdditionalCapability("app", WpfAppId);
+
+            session = new RemoteWebDriver(new Uri(WindowsApplicationDriverUrl), opt);
+
+            Actions builder = new Actions(session);
+
+
+            session.FindElementByName("Load file").Click();
+
+            builder.DoubleClick(session.FindElementByName("AutomaticTestGraph.json")).Perform();
+
+            session.Quit();
+
+
+        }
+
+
+    }
+}
